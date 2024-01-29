360054f8e6f16cb0df9d2c6df707faf8cdbd0920
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2024-01-29 16:27:17 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,6 +2,15 @@ const CANVAS_SIZE = 800;
 const NUMBER_OF_CELLS = 20;
 const CELL_SIZE = CANVAS_SIZE / NUMBER_OF_CELLS;
 const COLORS_ARRAY = ["#fae317", "#f50f0f", "#0d7fbe", "#f3f3f3", "#000000"];
+const POSSIBLE_SIZES = [
+  CELL_SIZE,
+  CELL_SIZE,
+  CELL_SIZE,
+  CELL_SIZE * 2,
+  CELL_SIZE * 3,
+  CELL_SIZE * 4,
+];
+
 
 function preload() {}
 
@@ -21,6 +30,8 @@ function setup() {
 
   // loop through canvas to create grid
   while (y < CANVAS_SIZE) {
+    //set height
+    const height = random(POSSIBLE_SIZES);
     //reset x
     x = 0;
     while (x < CANVAS_SIZE) {
@@ -31,17 +42,16 @@ function setup() {
       fill(cellColor);
 
       //calculate size
-      const width = CELL_SIZE;
-      const height = CELL_SIZE;
+      const width = random(POSSIBLE_SIZES);
 
       //draw image
       rect(x, y, width, height);
 
       //update x
-      x = x + CELL_SIZE;
+      x = x + width;
     }
     
     //update y
-    y = y + CELL_SIZE;
+    y = y + height;
   }
 }
